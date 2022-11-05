8fd9ae4b71ab6120a07af587feb4ed4ee7bbd725
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2022-11-05 20:10:57 +0100

    finishing minicart functionality and adding large desktop typography

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ const mobileResolution = window.matchMedia("(max-width: 1023px)")
       decreaseQtyButton = document.getElementById('decreaseQty')
       increaseQtyButton = document.getElementById('increaseQty')
       addToCartButton = document.getElementById('addToCart')
+      deleteFromCartButton = document.getElementById('deleteFromCart')
       cart = []
-
+      productImagesThumbnails = Array.from(document.getElementById('productImagesThumbnails').children)
+    
 let selectedQuantity = 1;
+    mainImage = document.getElementById('mainImg')
 
 function Product(name, price, qty, total) {
     this.name = name;
@@ -35,7 +38,7 @@ const toggleMobileMenu = () => {
 openMenuButton.addEventListener('click', toggleMobileMenu)
 closeMenuButton.addEventListener('click', toggleMobileMenu)
 
-menuOverlay.addEventListener('click', function(e){
+const closeMenuWhenClickingOutside = e => {
     const isOutside = !e.target.closest('.menu')
     //close mobile menu when clicking outside of it
     if (isOutside){
@@ -43,7 +46,9 @@ menuOverlay.addEventListener('click', function(e){
         menu.classList.remove('menu__active')
         menuOverlay.classList.remove('menu__overlay__active')
     }
-})
+}
+
+menuOverlay.addEventListener('click', closeMenuWhenClickingOutside)
 
 //if resolution is mobile and the menu is not hidden hide it otherwise show it on desktop
 const mobileOrDesktopMenu = () => {
@@ -64,6 +69,7 @@ const toggleMinicart = () => {
         minicart.classList.add('minicart__active')
         minicart.classList.remove('minicart__hidden')
         minicartButton.classList.add('button__minicart-active')
+        isMinicartEmpty()
     } else {
         minicart.classList.remove('minicart__active')
         minicart.classList.add('minicart__hidden')
@@ -73,31 +79,47 @@ const toggleMinicart = () => {
 
 minicartButton.addEventListener('click', toggleMinicart)
 
+const isMinicartEmpty = () => {
+    if (cart.length === 0) {
+        document.getElementById('cartEmpty').style.display = 'block'
+        document.getElementById('cartFull').style.display = 'none'
+    } else {
+        document.getElementById('cartEmpty').style.display = 'none'
+        document.getElementById('cartFull').style.display = 'block'
+        showMinicartContent()
+    }
+}
+
+const showMinicartContent = () => {
+   document.querySelector('.cart__product-name').innerHTML = cart[0].name;
+   document.querySelector('.cart__product-price-val').innerHTML = cart[0].price
+   document.querySelector('.cart__product-qty-val').innerHTML = cart[0].qty
+   document.querySelector('.cart__product-total-val').innerHTML = cart[0].total
+}
+
 decreaseQtyButton.addEventListener('click', function selectQty() {
         const qtyInput = document.querySelector("input[name='quantity']")
         qtyInput.stepDown()
-        updateCart(qtyInput.value)
+        selectedQuantity = parseInt(qtyInput.value);
     }
 )
 
 increaseQtyButton.addEventListener('click', function selectQty() {
         const qtyInput = document.querySelector("input[name='quantity']")
         qtyInput.stepUp()
-        updateCart(qtyInput.value)
+        selectedQuantity = parseInt(qtyInput.value);
     }
 )
 
-const updateCart = (qty) => {
-    console.log(qty)
-    selectedQuantity = qty;
+const updateCart = (quantity, total) => {
+    cart[0].qty = quantity
+    cart[0].total = total
+    toggleMinicart()
 }
 
-
-function Product(name, price, qty, total) {
-    this.name = name
-    this.price = price
-    this.qty = qty
-    this.total = total
+const deleteItemFromCart = () => {
+    cart.length = 0
+    isMinicartEmpty()
 }
 
 const addToCart = () => {
@@ -109,20 +131,31 @@ const addToCart = () => {
     const addedProduct = new Product(productName, price, quantity, total)
     const indexOfAddedProduct = cart.findIndex(object => object.id === addedProduct.id)
 
-    //add product to cart array if it doesn't already exist
-    if (indexOfAddedProduct === -1) {
-        cart.push(addedProduct);
+    //add product to cart array if it doesn't already exist or update quantity and total if it does
+   if (indexOfAddedProduct === -1) {
+       cart.push(addedProduct);
+       toggleMinicart()
+    } else if (indexOfAddedProduct !== 1){
+        updateCart(quantity, total)
     }
-
-    console.log(addedProduct, cart.length)
 }
 
 addToCartButton.addEventListener('click', addToCart)
+deleteFromCart.addEventListener('click', deleteItemFromCart)
 
+productImagesThumbnails.forEach(function(thumbnail){
+    thumbnail.addEventListener('click', function(){
+        thumbnail.classList.add('active')
+        const thumbnailSrc = thumbnail.getAttribute('src')
+        const selectedImageSrc = thumbnailSrc.replace('-thumbnail','')
+        mainImage.src = selectedImageSrc
+    })
+})
 
+const openLightBox = () => {
+    document.getElementById('productGallery').style.display = 'block'
+}
 
-   
-
-
+mainImage.addEventListener('click', openLightBox)
 
 
